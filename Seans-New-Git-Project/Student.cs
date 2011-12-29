a63baf817a2fb15091d7473d5f66d55acd091dec
Sean Jacob <sean.jacob@watchfinder.co.uk>
2011-12-29 09:38:30 +0000

    student class creation

---
+++ Seans-New-Git-Project/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Seans_New_Git_Project
+{
+    public class Student
+    {
+        
+        
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Surname {get;set;}
+        public string EmailAddress { get; set; }
+
+                
+    }
+
+
+
+
+    public class test
+    {
+
+        public test()
+        {
+            var mystudent = new Student();
+
+            mystudent.Id = 1;
+
+        }
+
+
+    }
+}
