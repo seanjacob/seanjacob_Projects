cacf37030c6fc180ebe73b6f9c3c73e2c5a4e9e1
Sean Jacob <sean.jacob@watchfinder.co.uk>
2011-12-29 10:41:31 +0000

    Added baby student class closes #1

--- Seans-New-Git-Project/Student.cs
+++ Seans-New-Git-Project/Student.cs
@@ -10,27 +10,60 @@ namespace Seans_New_Git_Project
         
         
         public int Id { get; set; }
-        public string FirstName { get; set; }
+        public virtual string FirstName { get; set; }
         public string Surname {get;set;}
         public string EmailAddress { get; set; }
         public string Telephone { get; set; } 
-        public string Address {get;set;}       
+        public string Address {get;set;}
+
+        public bool IsMature { get; set; } 
     }
 
+
+
+    public class MatureStudent : Student
+    {
+
+        public override string FirstName
+        {
+            get
+            {
+                var firstTmp = "SIR " +  base.FirstName;
+
+                return firstTmp;
+            }
+            set
+            {
+                base.FirstName = value;
+            }
+        }
+
+
+
+    }
+
+    public class BabyStudent : Student
+    {
+
+
+    }
 
 
 
     public class test
     {
 
+
         public test()
         {
-            var mystudent = new Student();
 
-            mystudent.Id = 1;
+            var test = new MatureStudent();
+
+
+             
 
         }
 
-
     }
+
 }
