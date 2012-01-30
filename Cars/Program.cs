116b05d76269a3561cebfbb132a023764f1828ff
Sean Jacob <sean.jacob@watchfinder.co.uk>
2012-01-30 14:53:58 +0000

    added console app

---
+++ Cars/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cars
+{
+    public  class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+        }
+    }
+}
